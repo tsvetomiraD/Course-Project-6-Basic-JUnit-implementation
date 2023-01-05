923f9c9694b1c750a71f3cc11acf06fec0b04421
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-05 10:12:22 +0200

    Update JUnit.java

--- JUnit.java
+++ JUnit.java
@@ -1,15 +1,18 @@
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 public class JUnit {
-    static Method beforeEach = null;
-    static Method afterEach = null;
-    static Method beforeClass = null;
-    static Method afterClass = null;
+    public static void main(String[] args) throws Exception {
+        run(args);
+    }
+    static List<Method> beforeEach =new ArrayList<>();
+    static List<Method> afterEach = new ArrayList<>();
+    static List<Method> beforeClass = new ArrayList<>();
+    static List<Method> afterClass =new ArrayList<>();
+    static List<Method> test = new ArrayList<>();
+    static Map<Method, Integer> repeatTest = new HashMap<>();
 
     public static void run(String[] classes) {
         try {
@@ -25,36 +28,87 @@ public class JUnit {
             throw new IllegalArgumentException("The object to serialize is null");
         }
 
-        checkBeforeAndAfter(cl);
-        List<Object> fieldsValues = new ArrayList<>();
-        for (Field field : cl.getDeclaredFields()) {
-            fieldsValues.add(field.get(cl.newInstance()));
+        getMethods(cl);
+        Object instancedClass = cl.getDeclaredConstructor().newInstance();
+
+        invokeMethods(instancedClass);
+    }
+
+    private static void invokeMethods(Object instancedClass) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : beforeClass) {
+            method.invoke(instancedClass);
         }
 
-        for (Method method : cl.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Test.class)) {
-                if (beforeEach != null) {
-                    beforeEach.setAccessible(true);
-                    beforeEach.invoke(cl.newInstance());
-                }
-                method.setAccessible(true);
-                method.invoke(cl.newInstance());
+        for (Method method : test) {
+            for (Method m : beforeEach) {
+                m.invoke(instancedClass);
+            }
 
-                if (afterEach != null) {
-                    afterEach.setAccessible(true);
-                    afterEach.invoke(cl.newInstance());
+            method.invoke(instancedClass);
+
+            for (Method m : afterEach) {
+                m.invoke(instancedClass);
+            }
+        }
+
+        for (Map.Entry<Method, Integer> method: repeatTest.entrySet()) {
+            for (int i = 0; i < method.getValue(); i++) {
+                for (Method m : beforeEach) {
+                    m.invoke(instancedClass);
+                }
+
+                method.getKey().invoke(instancedClass);
+
+                for (Method m : afterEach) {
+                    m.invoke(instancedClass);
                 }
             }
         }
+
+        for (Method method : afterClass) {
+            method.invoke(instancedClass);
+        }
     }
 
-    private static void checkBeforeAndAfter(Class<?> cl) {
-        beforeEach = (Method) Arrays.stream(cl.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(BeforeEach.class)).toArray()[0];
+    private static void getMethods(Class<?> cl) {
+        for (Method method : cl.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(BeforeClass.class) && Modifier.isStatic(method.getModifiers())) {
+                method.setAccessible(true);
+                beforeClass.add(method);
+            } else if (method.isAnnotationPresent(BeforeClass.class) && !Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException("@BeforeClass method must be static");
+            }
 
-        //afterEach = (Method) Arrays.stream(cl.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(AfterEach.class)).toArray()[0];
+            if (method.isAnnotationPresent(BeforeEach.class) && !Modifier.isStatic(method.getModifiers())) {
+                method.setAccessible(true);
+                beforeEach.add(method);
+            } else if (method.isAnnotationPresent(BeforeEach.class) && Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException("@BeforeEach method must not be static");
+            }
 
-        //beforeClass = (Method) Arrays.stream(cl.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(BeforeClass.class)).toArray()[0];
+            if (method.isAnnotationPresent(Test.class)) {
+                method.setAccessible(true);
+                test.add(method);
+            }
+            if (method.isAnnotationPresent(RepeatedTest.class)) {
+                method.setAccessible(true);
+                RepeatedTest rt = method.getAnnotation(RepeatedTest.class);
+                repeatTest.put(method, rt.times());
+            }
 
-        //afterClass = (Method) Arrays.stream(cl.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(AfterClass.class)).toArray()[0];
+            if (method.isAnnotationPresent(AfterClass.class) && Modifier.isStatic(method.getModifiers())) {
+                method.setAccessible(true);
+                afterClass.add(method);
+            } else if (method.isAnnotationPresent(AfterClass.class) && !Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException("@AfterClass method must be static");
+            }
+
+            if (method.isAnnotationPresent(AfterEach.class) && !Modifier.isStatic(method.getModifiers())) {
+                method.setAccessible(true);
+                afterEach.add(method);
+            } else if (method.isAnnotationPresent(AfterEach.class) && Modifier.isStatic(method.getModifiers())) {
+                throw new IllegalArgumentException("@AfterEach method must not be static");
+            }
+        }
     }
 }
